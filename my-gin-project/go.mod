09a492db1ca20bbc67cbb162aaafd42330c98a00
AdrienHouede <adrien.houede@gmail.com>
2025-10-01 17:01:03 +0200

    ajout connexion sentry

--- my-gin-project/go.mod
+++ my-gin-project/go.mod
@@ -3,12 +3,16 @@ module my-gin-project
 go 1.24.0
 
 require (
+	github.com/getsentry/sentry-go v0.35.3
+	github.com/getsentry/sentry-go/gin v0.35.3
 	github.com/gin-gonic/gin v1.11.0
 	github.com/golang-jwt/jwt/v5 v5.3.0
 	github.com/swaggo/files v1.0.1
 	github.com/swaggo/gin-swagger v1.6.1
+	github.com/swaggo/swag v1.16.6
 	golang.org/x/crypto v0.42.0
 	gorm.io/driver/mysql v1.6.0
+	gorm.io/driver/sqlite v1.6.0
 	gorm.io/gorm v1.31.0
 )
 
@@ -49,7 +53,6 @@ require (
 	github.com/pelletier/go-toml/v2 v2.2.4 // indirect
 	github.com/quic-go/qpack v0.5.1 // indirect
 	github.com/quic-go/quic-go v0.54.1 // indirect
-	github.com/swaggo/swag v1.16.6 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.3.0 // indirect
 	go.uber.org/mock v0.6.0 // indirect
@@ -62,5 +65,4 @@ require (
 	golang.org/x/text v0.29.0 // indirect
 	golang.org/x/tools v0.37.0 // indirect
 	google.golang.org/protobuf v1.36.9 // indirect
-	gorm.io/driver/sqlite v1.6.0 // indirect
 )
--- my-gin-project/src/main.go
+++ my-gin-project/src/main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"time"
+
 	"my-gin-project/src/models"
 	"my-gin-project/src/routes"
 
+	sentry "github.com/getsentry/sentry-go"
+	sentrygin "github.com/getsentry/sentry-go/gin"
 	"github.com/gin-gonic/gin"
 
-	_ "my-gin-project/src/docs" // Swagger docs
+	_ "my-gin-project/src/docs"
 )
 
 // @title My Gin API
@@ -19,12 +24,37 @@ import (
 // @in header
 // @name Authorization
 func main() {
+	// Initialisation de Sentry
+	err := sentry.Init(sentry.ClientOptions{
+		Dsn:              "https://2f1167ff3d20366cfa3695b14e6cb581@o4510114747121664.ingest.de.sentry.io/4510114754592848",
+		TracesSampleRate: 1.0, // pour activer le tracing, ajuster selon tes besoins
+	})
+	if err != nil {
+		fmt.Printf("Sentry initialization failed: %v\n", err)
+	}
+
+	defer sentry.Flush(2 * time.Second) // s'assure que les événements sont envoyés avant la fin du programme
+
 	// Initialisation DB
 	if err := models.InitDB(); err != nil {
 		log.Fatal("Failed to connect to database:", err)
 	}
 
 	r := gin.Default()
+
+	// Ajout du middleware Sentry
+	r.Use(sentrygin.New(sentrygin.Options{}))
+
+	r.GET("/panic", func(c *gin.Context) {
+		defer sentry.Recover() // capture un panic
+		panic("Something went wrong!")
+	})
+
+	r.GET("/custom-error", func(c *gin.Context) {
+		err := fmt.Errorf("une erreur personnalisée")
+		sentry.CaptureException(err)
+		c.JSON(500, gin.H{"error": err.Error()})
+	})
 
 	// Configuration des routes
 	routes.SetupRoutes(r)
